b7a5b1e1cc4bf57dbe22202aab0a07b216d2b9ea
Vincent Le <vinnyoodles@gmail.com>
2017-02-24 22:35:14 -0500

    add flip parenthesis problem/solution

---
+++ ctci/chapter3/flipParenthesis.js
@@ -0,0 +1,34 @@
+/**
+ * First, remove all the valid pairs of parenthesis.
+ * Then, the number of flips would be half of the remaining string.
+ * Time: O(n)
+ * Space: O(n)
+ */
+function flipParen(paren) {
+  var list = [];
+  for (var i = 0; i < paren.length; i ++) {
+  	if (paren.charAt(i) == '(') {
+  		list.push('(');
+  	} else if (paren.charAt(i) == ')' && list[list.length - 1] == '(') {
+  		list.pop();
+  	} else {
+  		list.push(paren.charAt(i));
+  	}
+  }
+
+  if (list.length == 0) return 0;
+
+  var open = 0;
+  var close = 0;
+  for (var j = 0; j < list.length; j ++) {
+  	if (list[j] == '(') {
+  		open ++;
+  	} else {
+  		close ++;
+  	}
+  }
+
+  return (open / 2) + (close / 2);
+}
+
+module.exports = flipParen;
